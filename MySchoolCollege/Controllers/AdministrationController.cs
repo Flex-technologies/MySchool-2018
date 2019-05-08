e66c9ce8b9af1a914f81a6692bb1b1b5f724a3cb
cfndiaye2 <cfndiaye@icloud.com>
2019-05-08 13:48:42 +0200

    intégration de l'année académique dans la vue classes

--- MySchoolCollege/Controllers/AdministrationController.cs
+++ MySchoolCollege/Controllers/AdministrationController.cs
@@ -10,7 +10,7 @@ using System.Web.Mvc;
 
 namespace MySchoolCollege.Controllers
 {
-    [Authorize(Roles ="Directeur,Surveillant")]
+    [Authorize(Roles = "Directeur,Surveillant")]
     public class AdministrationController : BaseController
     {
         private readonly ClasseRepository _classeRepository;
@@ -35,19 +35,28 @@ namespace MySchoolCollege.Controllers
         }
 
         //Gestion des classes
-        public ActionResult Classes(int? page)
+        public ActionResult Classes(int? page, int? annee)
         {
             var userOnLigne = Db.Users.Where(u => u.UserName == User.Identity.Name).FirstOrDefault();
             int id = 0;
-            if (userOnLigne.Etablissement != null) {
+            if (userOnLigne.Etablissement != null)
+            {
 
                 id = userOnLigne.Etablissement.Id;
-            } 
+            }
             var etablissement = userOnLigne.Etablissement; //_etablissementRepository.Get(id);
             List<ClasseViewModel> model = new List<ClasseViewModel>();
-            if(etablissement == null)
+            if (etablissement == null)
             {
-                model = _classeRepository.GetList().Select(c => c.ToViewModel()).ToList();
+                if (annee != 0)
+                {
+                    model = _classeRepository.GetList().Select(c => c.ToViewModel()).Where(c => c.AnneeScolaireId == annee).ToList();
+                }
+                else
+                {
+                    model = _classeRepository.GetList().Select(c => c.ToViewModel()).ToList();
+                }
+
             }
             else
             {
@@ -64,7 +73,7 @@ namespace MySchoolCollege.Controllers
             var etablissement = userOnLigne.Etablissement;
             var model = new ClasseViewModel();
 
-            model.Professeurs = Db.Users.Where(u1 => u1.Etablissement.Id == etablissement.Id).Where(u => u.Fonction.Id == "PROFESSEUR").Select(p => new SelectListItem { Text = p.Prenom + " " + p.Nom , Value = p.Id}).ToList();
+            model.Professeurs = Db.Users.Where(u1 => u1.Etablissement.Id == etablissement.Id).Where(u => u.Fonction.Id == "PROFESSEUR").Select(p => new SelectListItem { Text = p.Prenom + " " + p.Nom, Value = p.Id }).ToList();
             model.AnneeScolaires = _anneeScolaireRepository.GetList().Select(a => new SelectListItem { Text = a.AnneeAcademique, Value = a.Id.ToString() }).ToList();
             model.Etablissements = _etablissementRepository.GetList().Where(et => et.Id == etablissement.Id).Select(e => new SelectListItem { Text = e.Nom, Value = e.Id.ToString() }).ToList();
             model.ClasseDeBases = _classeDeBaseRepository.GetList().Select(c => new SelectListItem { Text = c.Nom, Value = c.Id.ToString() }).ToList();
@@ -90,10 +99,10 @@ namespace MySchoolCollege.Controllers
 
 
                 //On vérifie si la classe n'est pas déjà créée
-                var xclasse = Db.Classes.Where(c => (c.AnneeScolaire.Id == model.AnneeScolaireId) && (c.Etablissement.Id == model.EtablissementId) && (c.ClasseDeBase.Id == model.ClasseDeBaseId)).FirstOrDefault(); 
-                if(xclasse != null)
+                var xclasse = Db.Classes.Where(c => (c.AnneeScolaire.Id == model.AnneeScolaireId) && (c.Etablissement.Id == model.EtablissementId) && (c.ClasseDeBase.Id == model.ClasseDeBaseId)).FirstOrDefault();
+                if (xclasse != null)
                 {
-                    ModelState.AddModelError("","La classe est déjà ajoutée");
+                    ModelState.AddModelError("", "La classe est déjà ajoutée");
                 }
                 else
                 {
@@ -110,7 +119,7 @@ namespace MySchoolCollege.Controllers
 
                     return RedirectToAction("Classes");
                 }
-               
+
             }
 
             //Si on arrive ici ça veut dire qu'il ya un probléme
@@ -126,24 +135,24 @@ namespace MySchoolCollege.Controllers
         public ActionResult ModifierClasse(int id)
         {
             var classe = _classeRepository.Get(id);
-            if(classe == null)
+            if (classe == null)
             {
                 return HttpNotFound();
             }
             var model = classe.ToViewModel();
             var userOnLigne = Db.Users.Where(u => u.UserName == User.Identity.Name).First();
             var etablissement = userOnLigne.Etablissement;
-            if(etablissement != null)
+            if (etablissement != null)
             {
                 model.Professeurs = Db.Users.Where(u1 => u1.Etablissement.Id == etablissement.Id).Where(u => u.Fonction.Id == "PROFESSEUR").Select(p => new SelectListItem { Text = p.Prenom + " " + p.Nom, Value = p.Id }).ToList();
                 model.Etablissements = _etablissementRepository.GetList().Where(et => et.Id == etablissement.Id).Select(e => new SelectListItem { Text = e.Nom, Value = e.Id.ToString() }).ToList();
-            } 
+            }
             else
             {
                 model.Professeurs = Db.Users.Where(u => u.Fonction.Id == "PROFESSEUR").Select(p => new SelectListItem { Text = p.Prenom + " " + p.Nom, Value = p.Id }).ToList();
                 model.Etablissements = _etablissementRepository.GetList().Select(e => new SelectListItem { Text = e.Nom, Value = e.Id.ToString() }).ToList();
             }
-            
+
             model.AnneeScolaires = _anneeScolaireRepository.GetList().Select(a => new SelectListItem { Text = a.AnneeAcademique, Value = a.Id.ToString() }).ToList();
             model.ClasseDeBases = _classeDeBaseRepository.GetList().Select(c => new SelectListItem { Text = c.Nom, Value = c.Id.ToString() }).ToList();
 
@@ -156,7 +165,7 @@ namespace MySchoolCollege.Controllers
             var userOnLigne = Db.Users.Where(u => u.UserName == User.Identity.Name).First();
             if (ModelState.IsValid)
             {
-                
+
                 model.ModifierLe = DateTime.Now;
                 model.Modifierpar = userOnLigne;
                 model.ProfesseurPrincipal = Db.Users.Where(u => u.Id == model.ProfesseurPrinciplaId).FirstOrDefault();
@@ -207,11 +216,11 @@ namespace MySchoolCollege.Controllers
             return View(model);
         }
 
-        [Authorize(Roles ="Administrateur")]
+        [Authorize(Roles = "Administrateur")]
         public ActionResult SupprimerClasse(int id)
         {
             var classe = _classeRepository.Get(id);
-            if(classe == null)
+            if (classe == null)
             {
                 return HttpNotFound();
             }
@@ -220,7 +229,7 @@ namespace MySchoolCollege.Controllers
             return View(model);
         }
 
-        [Authorize(Roles="Administrateur")]
+        [Authorize(Roles = "Administrateur")]
         [HttpPost]
         public ActionResult SupprimerClasseConfirmation(int id)
         {
@@ -240,7 +249,7 @@ namespace MySchoolCollege.Controllers
         {
             ClasseViewModel model = new ClasseViewModel();
             var classe = _classeRepository.Get(id);
-            if(classe == null)
+            if (classe == null)
             {
                 return HttpNotFound();
             }
